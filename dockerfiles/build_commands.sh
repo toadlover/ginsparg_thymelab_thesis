d395b6ed5d9a7963e32e8d325ffae177771b5950
Ari Benjamin Ginsparg <abgvg9@uab.edu>
2024-07-23 17:23:40 -0400

    Update build_commands.sh

--- dockerfiles/build_commands.sh
+++ dockerfiles/build_commands.sh
@@ -1,12 +1,18 @@
 #Using docker version 24.0.5 and singularity version 4.1.0
+#$1 is the name of the container that you want, which will pass down all steps
 
-#build shapedb container:
-sudo docker build - < Dockerfile -t shapedb_container:1.2.1 --progress=plain
+#build container:
+echo Making container with name: $1
+sudo docker build - < Dockerfile -t $1 --progress=plain
 
 #get image id:
+image_id=$(sudo docker images | grep $1 | awk '{print $3}')
+echo $1 image id: $image_id
 
 #write image to tar that singularity can use:
-sudo docker save $IMAGE_ID -o shapedb_container.tar
+echo Saving image to tar
+sudo docker save $image_id -o $1.tar
 
 #build sif container from image
-sudo singularity build shapedb_container.sif docker-archive://shapedb_container.tar
+echo Converting tar to Singularity SIF: $1.sif
+sudo singularity build $1.sif docker-archive://$1.tar
