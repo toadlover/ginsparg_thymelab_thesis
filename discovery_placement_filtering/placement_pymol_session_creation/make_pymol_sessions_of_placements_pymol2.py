323ed318efd865cca9614f48d92ffcf6274c62e2
Ginsparg <abgvg9@uab.edu>
2024-07-30 10:42:27 -0400

    attempt to count hydrogen bonds using new method

--- discovery_placement_filtering/placement_pymol_session_creation/make_pymol_sessions_of_placements_pymol2.py
+++ discovery_placement_filtering/placement_pymol_session_creation/make_pymol_sessions_of_placements_pymol2.py
@@ -84,7 +84,13 @@ with pymol2.PyMOL() as pymol:
             #pymol.cmd.set('dash_color', 'green', f'{object_name}_hbonds')
             pymol.cmd.set('dash_width', 2.0)
 
-            print(f'{object_name}_hbonds')
+            # Count the number of measurements (bonds) in the hydrogen bond object
+            try:
+                count = 0
+                pymol.cmd.iterate_state(1, f'{object_name}_hbonds', 'count += 1', space={'count': count})
+                print(f"Number of measurements (bonds) in {object_name}_hbonds: {count}")
+            except Exception as e:
+                print(f"Error counting bonds for {object_name}. Error: {e}")
 
              # Print names of all active objects
             active_objects = pymol.cmd.get_names()
