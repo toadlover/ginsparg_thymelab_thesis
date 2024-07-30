b48d6488731920b9028bd1bd0a13458510cf6e4b
Ginsparg <abgvg9@uab.edu>
2024-07-30 10:08:56 -0400

    bug fix

--- discovery_placement_filtering/placement_pymol_session_creation/make_pymol_sessions_of_placements_pymol2.py
+++ discovery_placement_filtering/placement_pymol_session_creation/make_pymol_sessions_of_placements_pymol2.py
@@ -80,7 +80,7 @@ with pymol2.PyMOL() as pymol:
             # Display hydrogen bonds
             pymol.cmd.dist(f'{object_name}_hbonds', f'{object_name} and {ligand_selection}', neighboring_residues, cutoff=3.5, mode=2)
             pymol.cmd.set('dash_color', 'yellow', f'{object_name}_hbonds')
-            pymol.cmd.dist(f'{object_name}_hbonds', f'{object_name} and {ligand_selection}', highlight_residues, cutoff=3.5, mode=2)
+            pymol.cmd.dist(f'{object_name}_hbonds', f'{object_name} and {ligand_selection}', f'resi {ligand_selection}', cutoff=3.5, mode=2)
             pymol.cmd.set('dash_color', 'green', f'{object_name}_hbonds')
             pymol.cmd.set('dash_width', 2.0)
 
