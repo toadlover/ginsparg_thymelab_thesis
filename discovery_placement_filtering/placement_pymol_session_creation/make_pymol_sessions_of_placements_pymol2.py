9b67e776b9f3216d6db42da3150607d71dab907e
Ginsparg <abgvg9@uab.edu>
2024-07-30 10:04:15 -0400

    attempt to make separate hbond session and unique coloring for when against significant residue

--- discovery_placement_filtering/placement_pymol_session_creation/make_pymol_sessions_of_placements_pymol2.py
+++ discovery_placement_filtering/placement_pymol_session_creation/make_pymol_sessions_of_placements_pymol2.py
@@ -79,7 +79,8 @@ with pymol2.PyMOL() as pymol:
 
             # Display hydrogen bonds
             pymol.cmd.dist(f'{object_name}_hbonds', f'{object_name} and {ligand_selection}', neighboring_residues, cutoff=3.5, mode=2)
-            pymol.cmd.set('dash_color', 'green', f'{object_name}_hbonds')
+            pymol.cmd.set('dash_color', 'green', f'{object_name}_hbonds and resi {highlight_residues}')
+            pymol.cmd.set('dash_color', 'yellow', f'{object_name}_hbonds')
             pymol.cmd.set('dash_width', 2.0)
 
              # Print names of all active objects
@@ -96,21 +97,74 @@ with pymol2.PyMOL() as pymol:
 
     # Save the session for all proteins
     pymol.cmd.save('all_proteins_session.pse')  # Save the PyMOL session
-"""
-    # Save a new session containing only hydrogen bond objects
-    # Create a new session file for hydrogen bonds
-    pymol.cmd.delete('all')  # Remove all objects from the current session
-    
-    # Reload the original session
-    pymol.cmd.load('all_proteins_session.pse')
 
-    # Create a selection for hydrogen bond objects
-    all_objects = [obj for obj in pymol.cmd.get_names()]
-    print(all_objects)
-    
-    for obj in all_objects:
-        if obj.endswith("_hbond") == False:
-            pymol.cmd.delete(obj)
 
-    pymol.cmd.save('hbonds_only_session.pse')
-"""
+# Initialize PyMOL in headless mode for hbonds only
+with pymol2.PyMOL() as pymol:
+    
+    #set the internal gui width
+    pymol.cmd.set('internal_gui_width', 600)
+
+    # Loop through each file in the directory
+    for filename in os.listdir(directory):
+        if filename.endswith('.pdb'):  # Ensure only PDB files are processed
+            filepath = os.path.join(directory, filename)
+            
+            # Load the file
+            pymol.cmd.load(filepath)
+            
+            # Get the object name (assumed to be the filename without the extension)
+            object_name = os.path.splitext(filename)[0]
+            print(object_name)
+            
+            # Color the entire protein
+            pymol.cmd.color(protein_color, object_name)
+
+            
+            # Find and show residues within a certain distance of the ligand
+            neighboring_residues = f'(byres {ligand_selection} around {distance_threshold}) and {object_name}'
+            pymol.cmd.show('sticks', neighboring_residues)
+            #pymol.cmd.color('elem', neighboring_residues)
+            pymol.cmd.color('yellow', f'{neighboring_residues} and elem C')
+
+            # Show the specific residues as sticks and color them
+            pymol.cmd.show('sticks', f'{object_name} and resi {highlight_residues}')
+            #pymol.cmd.color('elem', f'{object_name} and resi {highlight_residues}')
+            pymol.cmd.color('orange', f'{object_name} and resi {highlight_residues} and elem C')
+
+            #color the ligands magenta
+            pymol.cmd.color('white', f'{ligand_selection} and elem C')
+
+            #fixing element coloring:
+            pymol.cmd.color('white', 'elem H')
+            pymol.cmd.color('red', 'elem O')
+            pymol.cmd.color('blue', 'elem N')
+            pymol.cmd.color('green', 'elem Cl')
+            pymol.cmd.color('cyan', 'elem F')
+            pymol.cmd.color('brown', 'elem Br')
+            pymol.cmd.color('purple', 'elem I')
+            pymol.cmd.color('yellow', 'elem S')
+
+            # Display hydrogen bonds
+            pymol.cmd.dist(f'{object_name}_hbonds', f'{object_name} and {ligand_selection}', neighboring_residues, cutoff=3.5, mode=2)
+            pymol.cmd.set('dash_color', 'green', f'{object_name}_hbonds')
+            pymol.cmd.set('dash_width', 2.0)
+
+             # Print names of all active objects
+            active_objects = pymol.cmd.get_names()
+            #print(f"Active objects in the session: {active_objects}")
+
+            #attempt to count the number of measurements om the hydrogen bond distance object
+            #distance = pymol.cmd.get_distance(f'{object_name}_hbonds')
+            #print(distance)
+
+            # Group the molecule and the hydrogen bond distance objects together
+            #pymol.cmd.group(f'{object_name}_group', f'{object_name} {object_name}_hbonds')
+
+            #delete the molecule object so we only keep the hbond
+            pymol.cmd.delete(f'{object_name}')
+
+
+
+    # Save the session for all proteins
+    pymol.cmd.save('all_proteins_session.pse')  # Save the PyMOL session
