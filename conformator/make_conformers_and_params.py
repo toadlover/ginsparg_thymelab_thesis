a61b8eaaeb053c9f048bbbae3d16c3bbad7a1efe
Ginsparg <abgvg9@uab.edu>
2024-08-15 12:33:33 -0400

    fixing syntax error

--- conformator/make_conformers_and_params.py
+++ conformator/make_conformers_and_params.py
@@ -8,7 +8,7 @@ license_key = sys.argv[2]
 
 #split name to remove extension
 #if there is a path, remove that too to work in the current location
-starting_file_no_ext = starting_file.split("/")[len(split("/")) - 1].split(".")[0]
+starting_file_no_ext = starting_file.split("/")[len(starting_file.split("/")) - 1].split(".")[0]
 
 #make a directory to perform all operations in and then move into it
 os.system("mkdir " + starting_file_no_ext)
