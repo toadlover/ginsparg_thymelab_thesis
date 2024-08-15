9bb36cbb66a154aaf61f1f86e2f7b53d48cbb5da
Ginsparg <abgvg9@uab.edu>
2024-08-15 16:38:29 -0400

    fixing script to work more universaslly

--- shell_scripts/make_db_and_condense_params.sh
+++ shell_scripts/make_db_and_condense_params.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 
 #set -e
-#$3 is sub_directory number (0-9)
-#$4 is the directory (00000-53084)
+#$1 is the file
 
-#set up pip and import relevant packages (may need to incorporate this into the base container)
-#apt-get update
-#apt-get install python-pip -y
-#pip install numpy
-#pip install openbabel
+#get the absolute path to make_db_and_condense_params.sh for use in calling some other scripts in the github repo
+# Save the current working directory
+original_dir=$(pwd)
 
+# Get the directory where the script is located
+script_dir=$(dirname "$(realpath "$0")")
 
-#untar the split_new_named directory that contains the conformers in sdf and params format, as well as the text file list containing the name of every conformer (lig name + conformer id number) to be used for searching
-tar -xzvf split_new_named_$3.tar.gz
+# Change to the script's directory
+cd "$script_dir"
+
+# Find the root of the GitHub repository
+repo_root=$(git rev-parse --show-toplevel)
+
+# Return to the original directory
+cd "$original_dir"
+
+# Now `repo_root` holds the path to the root of the GitHub repository
+echo "The root of the GitHub repository is: $repo_root"
+
+#derive the base directory name without the .tar.gz
+dirname=$(basename "$tarfile" .tar.gz)
+
+#untar the ligand confs directory that contains the conformers in sdf and params format, as well as the text file list containing the name of every conformer (lig name + conformer id number) to be used for searching
+tar -xzf $1
 #move into the directory
-cd split_new_named_$3
+cd $dirname
 
 #delete the single sdf file folder (we don't need it)
 rm -drf single_conf_sdfs
 
+#prepare anaconda
 whoami
 . /opt/miniconda3/bin/activate
 env "PATH=$PATH" conda update conda
@@ -31,7 +46,7 @@ env "PATH=$PATH" conda update conda
 #params handling
 #move into the params directory and then run the params condense script
 cd single_conf_params
-python3 ../../condense_conformer_params_data.py
+python3 $repo_root/params_file_compression/condense_conformer_params_data.py
 #text files of condensed params should be generated. We can delete the .params to save on a lot of space
 rm *.params
 
@@ -41,7 +56,7 @@ cd ..
 #==========================================================================
 #use shapedb to make database (db.db)
 #run align.py on the confs_named.sdf file, name as aligned.sdf
-python ../align.py confs_named.sdf aligned.sdf
+python $repo_root/shapedb/align.py confs_named.sdf aligned.sdf
 
 #run shapedb on aligned.sdf to create the database
 /pharmit/src/build/shapedb -h -Create -in aligned.sdf -db db.db
@@ -49,18 +64,12 @@ python ../align.py confs_named.sdf aligned.sdf
 #we no longer need confs_named or aligned.sdf
 rm confs_named.sdf aligned.sdf
 
-#rename the conformer name text file
-mv split_new_named_$3_lig_name_list.txt $4_$3_lig_name_list.txt
-
-#copy the file up a level
-#cp $4_$3_lig_name_list.txt ..
-
 #move up a level
 cd ..
 
-#rename and compress the folder
-mv split_new_named_$3 condensed_params_and_db_$3
-tar -czvf condensed_params_and_db_$3.tar.gz condensed_params_and_db_$3
+#recompress the folder
+
+tar -czf $1 $dirname
 
 #compress the name list
 #tar -czvf $4_$3_lig_name_list.txt.tar.gz $4_$3_lig_name_list.txt
