a01aa3a97da51686e4232347aa9700df258e4666
Ginsparg <abgvg9@uab.edu>
2024-08-19 17:30:43 -0400

    bug fix calling wrong line variable

--- discovery_placement_filtering/placement_scoring/conformer_refinement_analysis.py
+++ discovery_placement_filtering/placement_scoring/conformer_refinement_analysis.py
@@ -141,7 +141,7 @@ for line in placements_csv.readlines():
 		#set up dictionary for the score terms from the compare file
 		compare_conf_dict = {}
 		term_counter = 0
-		for item in line.split(","):
+		for item in compare_line.split(","):
 			score_term_names.append(item.strip())
 			#add the term to the dictionary
 			compare_conf_dict[score_term_names[term_counter]] = item.strip()
