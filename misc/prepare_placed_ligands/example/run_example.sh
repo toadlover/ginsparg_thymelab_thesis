511fd179b7799501e9a986ef32cf9dcdaab6254c
Ari Benjamin Ginsparg <abgvg9@login004.cm.cluster>
2024-10-09 11:54:12 -0500

    adding example files and test case

---
+++ misc/prepare_placed_ligands/example/run_example.sh
@@ -0,0 +1,2 @@
+#note: you need to run this script in an environment with rdkit and openbabel (looking for an executable named obabel)
+python ../prepare_ligands_from_pdb_for_enamine_order.py example_ligands_file.txt example_output.sdf example_output.csv
