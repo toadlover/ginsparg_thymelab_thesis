0eb31a3d4f9e7b93b5ea68a07d071cf033a10db0
Ginsparg <abgvg9@uab.edu>
2024-12-20 14:01:40 -0500

    adding script to get smiles of target ligand file to misc for alphafold  usage

---
+++ misc/alphafold_prep/get_smiles_of_ligand_file.py
@@ -0,0 +1,36 @@
+import os,sys
+
+#use Chem in rdkit to derive SMILES string of placed ligands
+from rdkit import Chem
+
+#get ligand file (and path) that you want to derive smiles for
+#smiles file that is written will take the name up to the first period
+#for the sake of simplicity, this only works for mol2 and sdf ligands
+input_file = sys.argv[1]
+
+#sanity check for file type
+if input_file.endswith(".sdf") == False and input_file.endswith(".mol2") == False:
+	print("File input of " + input_file + " is not of type sdf or mol2, and we will not mess with it")
+	quit()
+
+file_prefix = input_file.split(".")[0]
+
+#optional, location to write the smiles file to. otherwise default to writing to current location
+output_path = ""
+if len(sys.argv >=2):
+	output_path = sys.argv[2]
+
+	#append a slash to output_path if it does not end with one
+	if output_path.endswith("/") == False:
+		output_path = output_path + "/"
+
+#derive the smiles string
+supplier = Chem.SDMolSupplier(input_file)
+for mol in supplier:
+	if mol is not None:  # Check for valid molecule
+		smiles = Chem.MolToSmiles(mol)
+
+		#write the smiles to a new file
+		write_file = open(output_path + file_prefix + ".smi","w")
+		write_file.write(smiles)
+		write_file.close()
