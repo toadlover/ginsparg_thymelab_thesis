0bd5202e482a77465db86ca058e0709f9d43503c
Ginsparg <abgvg9@uab.edu>
2025-01-13 16:37:26 -0500

    expanding code for analysis

--- aligned_hcrtr2_pdbs_for_analysis/get_rmsd_of_select_residues_of_aligned_hcrtr2_structures.py
+++ aligned_hcrtr2_pdbs_for_analysis/get_rmsd_of_select_residues_of_aligned_hcrtr2_structures.py
@@ -24,6 +24,77 @@ for line in pocket_residue_file.readlines():
 		residues_of_interest.append([residue,index])
 
 #sanity check print of the residues of interest
+#also make a list of the residue plus index for output
+
+residues_of_interest_single_entry = []
+
 for index in residues_of_interest:
 	print(index)
+	residues_of_interest_single_entry.append(str(index[0]) + "_" + str(index[1]))
 
+
+#indexing should be the same for all atoms
+
+#create dictionary that holds all atom data for each atom from residues of interest from each system
+systems_atom_data = {}
+
+#now, read through each local pdb file and get the rmsd of the residues of interest against 4s0v's residues
+#the call to os.getcwd() means that this script is intended to be ran in ginsparg_thymelab_thesis/aligned_hcrtr2_pdbs_for_analysis
+for r,d,f in os.walk(os.getcwd()):
+	for file in f:
+		#make sure the file is in the working location and the file is a pdb file and that aligned is in the file name
+		if r == os.getcwd() and file.endswith("_aligned.pdb"):
+			#extract the file name
+			file_base_name = file.split("_")[0]
+			print(file_base_name)
+
+			#prepare systems dictionary for this system
+			#add a dictionary using the base name as a key, and this nested dictionary will use the residue indices as keys
+			systems_atom_data[file_base_name] = {}
+
+			#read through the file and get the atom data from the residues of interest
+			curr_file = open(file,"r")
+
+			for line in curr_file.readlines():
+				#only work with lines starting with ATOM
+				if line.startswith("ATOM") == False:
+					continue
+
+				#check if the line corresponds to an residue index of interest
+				#note, this method has potential to fail if the residue indices are 1000 or more, as chain id and residue index are no longer separated by a space
+				#for this specific study, this easier method will work, but would need to be reworked if dealing with indices this high
+				curr_index = line.split()[5]
+
+				is_intersting = False
+				#look through indices of interest
+				for this_ind in residues_of_interest_single_entry:
+					if curr_index == this_ind[1]:
+						#note is interesting and we will process
+						is_intersting = True
+
+				#do not process if not interesting
+				if is_intersting == False:
+					continue
+
+				#get the residue data
+				#if this is the first atom, create a new dictionary entry
+				if curr_index not in systems_atom_data[file_base_name].keys():
+					systems_atom_data[file_base_name][curr_index] = {}
+
+				#write the atom as a key (values should be unique) and coordinates as values
+				atom_name = line.split()[2]
+				atom_x = line.split()[6]
+				atom_y = line.split()[7]
+				atom_z = line.split()[8]
+
+				systems_atom_data[file_base_name][curr_index][atom_name] = [float(atom_x),float(atom_y),float(atom_z)]
+
+#system atom data should be filled now
+#test print
+for key in systems_atom_data.keys():
+	print(systems_atom_data[key])
+	print(len(systems_atom_data))
+
+
+
+		
