05750ddde5cdb75fd47daa1e21f47d8e3e1e02cd
Ginsparg <abgvg9@uab.edu>
2025-01-28 16:22:49 -0500

    adjusting so that there is a sort at the end

--- conformer_library_ligand_similarity/run_smiles_comparison_check_on_superchunk_of_library.py
+++ conformer_library_ligand_similarity/run_smiles_comparison_check_on_superchunk_of_library.py
@@ -137,5 +137,8 @@ best_ligs_file = open("superchunk_top_" + str(top_ligand_amount) + ".csv", "w")
 
 print("Done collecting all data, writing top " + str(top_ligand_amount) + " closest matching ligands from this superchunk")
 
-for lig in top_ligands:
-	best_ligs_file.write(str(lig[1][0]) + "," + str(lig[1][1]) + "," + str(lig[1][2]) + "," + str(float(lig[1][3])) + "," + str(lig[1][4]) + "\n")
+#sort the heap to new list
+sorted_top_ligands = [heapq.heappop(top_ligands) for _ in range(len(top_ligands))]
+
+for lig in sorted_top_ligands:
+	best_ligs_file.write(str(lig[0]) + "," + str(lig[1]) + "," + str(lig[2]) + "," + str(float(lig[3])) + "," + str(lig[4]) + "\n")
