729d6f830769dacb05960152cc6963c40a49dcf3
Ginsparg <abgvg9@uab.edu>
2025-03-05 10:52:25 -0500

    removing properties adjustment to the ligand, only adjust the fragment

--- conformer_library_ligand_similarity/identify_ligands_that_contain_smiles_fragment_from_superchunk_data.py
+++ conformer_library_ligand_similarity/identify_ligands_that_contain_smiles_fragment_from_superchunk_data.py
@@ -85,7 +85,7 @@ for r,d,f in os.walk(library_location + str(subchunk)):
 				Chem.RemoveStereochemistry(lig_smiles)
 
 				#add the makedummies params to the full ligand being investigated
-				lig_smiles = AdjustQueryProperties(lig_smiles, params)
+				#lig_smiles = AdjustQueryProperties(lig_smiles, params)
 
 				#run the match of the fragment in the full ligand
 				if lig_smiles.HasSubstructMatch(fragment_molecule):
