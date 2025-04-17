a938aa2d94b484da5f8573d4f095fe4bcec92337
Ginsparg <abgvg9@uab.edu>
2025-04-17 12:38:51 -0400

    adding debugging line because rmsd values are incorrect

--- benchmarking_other_papers/5c8s_placement_analysis/rmsd_analysis.py
+++ benchmarking_other_papers/5c8s_placement_analysis/rmsd_analysis.py
@@ -79,6 +79,8 @@ for r,d,f in os.walk("5c8s_publication_placements"):
 							total_distance = total_distance + a_a_distance
 							atom_counter = atom_counter + 1
 
+							print(a_a_distance,atom_counter)
+
 						#derive the rmsd as the total over the atom count
 						rmsd = total_distance / atom_counter
 
